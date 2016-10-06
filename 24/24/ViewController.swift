a01a04d363d601893d8a8dcec81cea604f8980f2
Belinda Zeng <belindazeng@college.harvard.edu>
2016-10-06 01:03:39 -0700

    Rational numbers added

--- 24/24/ViewController.swift
+++ 24/24/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+// Rational fractions.
+typealias Rational = (num : Int, den : Int)
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var answerNumber1Label: UILabel!
@@ -78,6 +81,29 @@ class ViewController: UIViewController {
         answersFilled = 0
     }
     
+    // Helper functions
+    func rationalApproximationOf(x0: Double, withPrecision eps: Double = 1.0E-6) -> Rational {
+        var x: Double = x0
+        var a: Double = floor(x)
+        var (h1, k1, h, k): (Int, Int, Int, Int) = (1, 0, Int(a), 1)
+        
+        while x - a > eps * Double(k) * Double(k) {
+            x = 1.0/(x - a)
+            a = floor(x)
+            (h1, k1, h, k) = (h, k, h1 + Int(a) * h, k1 + Int(a) * k)
+        }
+        return (h, k)
+    }
+    
+    func formatAsString(num: Double) -> String {
+        let res: Rational = rationalApproximationOf(num)
+        if res.den == 1 {
+            return String(res.num)
+        }
+        
+        return String(format:"%d/%d", res.num, res.den)
+    }
+    
     func computeAnswer() {
         let numberButtons = [number1Button, number2Button, number3Button, number4Button]
         
@@ -110,12 +136,11 @@ class ViewController: UIViewController {
             }
             answer = number1! / number2!
         }
-        numberButtons[Int(answerNumber2Label.tag)].setTitle(String(format:"%.1f",answer), forState: .Normal)
         
-            numberButtons[Int(answerNumber2Label.tag)].setBackgroundImage(UIImage(named:"tile_large")!, forState: .Normal)
+        numberButtons[Int(answerNumber2Label.tag)].setTitle(formatAsString(answer), forState: .Normal)
         
-        print(answer)
-        
+        numberButtons[Int(answerNumber2Label.tag)].setBackgroundImage(UIImage(named:"tile_large")!, forState: .Normal)
+    
         clearAnswers()
         
         // decrement numbers left
