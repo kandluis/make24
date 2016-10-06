a93319d47e798e563648b5dad62b22a109c8e108
Luis Perez <luis.perez.live@gmail.com>
2016-10-05 17:52:18 -0700

    updatE

---
+++ 24/24Tests/_4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _4Tests.swift
+//  24Tests
+//
+//  Created by Luis Perez on 10/5/16.
+//  Copyright © 2016 Luis PerezBunnyLemon. All rights reserved.
+//
+
+import XCTest
+@testable import _4
+
+class _4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
